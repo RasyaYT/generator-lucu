cd991a952e11ffda12c9e9a49d50fd101179057d
RASYA <rasyagaming99@gmail.com>
2023-04-15 13:22:24 +0700

    s8

--- Sesi 8/index.js
+++ Sesi 8/index.js
@@ -1,32 +1,36 @@
-let passwordLength = document.getElementById("passwordLength")
-let password = document.getElementById("password")
-let saveButton = document.getElementById("saveButton")
+let passwordLength = document.getElementById("passwordLength");
+let password = document.getElementById("password");
+let saveButton = document.getElementById("saveButton");
 
 function generatePassword(len) {
-const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
-const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numberic = "0123456789"
-const symbol = "!@#$%^&*()_+=-[]{};:,.<>/?'`~"
+  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberic = "0123456789";
+  const symbol = "!@#$%^&*()_+=-[]{};:,.<>/?'`~";
 
-const data = lowerAlphabet + upperAlphabet + numberic + symbol
-let generator = '';
-for (let index = 0; index < len; index++) {
+  const data = lowerAlphabet + upperAlphabet + numberic + symbol;
+  let generator = "";
+  for (let index = 0; index < len; index++) {
     generator += data[~~(Math.random() * data.length)];
-    }
-    return generator
-// console.log('generator: ', generator);
+  }
+  return generator;
+  // console.log('generator: ', generator);
 }
 function getPassword() {
-    const newPassword = generatePassword(passwordLength.value)
-    password.value = newPassword
-    alert('password has been generate!')
+  const newPassword = generatePassword(passwordLength.value);
+  password.value = newPassword;
+  alert("password has been generate!");
 }
 
 function savePassword() {
-    document.title = password.value
-    saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
-    saveButton.setAttribute('download', 'MyPasswordGeneratorLOG.txt')
-    setTimeout(() => {
-        alert('berhasil disimpan')
-    }, 1000)
+  document.title = password.value;
+  saveButton.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," +
+      encodeURIComponent(`password saya: ${document.title}`)
+  );
+  saveButton.setAttribute("download", "MyPasswordGeneratorLOG.txt");
+  setTimeout(() => {
+    alert("berhasil disimpan");
+  }, 1000);
 }
